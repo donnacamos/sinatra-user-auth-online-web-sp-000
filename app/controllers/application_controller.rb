76c96e3be7fedd79502808e0f6981c304cdfe686
Donna Amos <donnacamos.developer@gmail.com>
2019-07-12 00:46:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    
+     user = User.find_by(:email => email)
+      if user && user.authenticate(password) 
+        session[:email] = user.email
+      else 
+        redirect '/login' 
+      end 
     # @user = User.find_by(email: params["email"], password: params["password"])
     # session[:id] = @user.id
     # redirect '/users/home'
