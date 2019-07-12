b49a3fa425023cccf279d4c06875cde4942c184a
Donna Amos <donnacamos.developer@gmail.com>
2019-07-12 00:46:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-     user = User.find_by(:email => email)
+     @user = User.find_by(:email => email)
       if user && user.authenticate(password) 
         session[:email] = user.email
       else 
